51faf4563e01f964856d7561d7df6326f6642a48
CaDil91 <caleb.dilworth@gmail.com>
2023-03-26 15:31:01 -0500

    Initial Commit. Rider thinks I need these in git... I don't think I do, but don't want to sort everything at once atm.

--- DiscordBot.cs
+++ DiscordBot.cs
@@ -1,5 +1,31 @@
+using Discord;
+using Discord.WebSocket;
+
 namespace JustADiscordBot;
 
 public class DiscordBot
 {
+    private readonly DiscordSocketClient _client;
+
+    public DiscordBot()
+    {
+        //When working with events that have Cacheable<IMessage, ulong> parameters, you must enable the message cache in your config settings if you plan to use the cached message entity.
+        var discordSocketConfig = new DiscordSocketConfig { MessageCacheSize = 100 };
+        _client = new DiscordSocketClient(discordSocketConfig);
+    }
+    
+    public async Task Execute()
+    {
+        
+        
+    }
+
+    /// <summary>
+    /// Check if DiscordBotSocketClient.ConnectionState.Connected == True.
+    /// </summary>
+    /// <returns>bool</returns>
+    public bool IsConnected()
+    {
+        return _client.ConnectionState == ConnectionState.Connected;
+    }
 }
---
+++ JustADiscordBotTests/DiscordBotTests.cs
@@ -0,0 +1,18 @@
+﻿namespace JustADiscordBot;
+
+public class DiscordBotTests
+{
+    [Fact]
+    public async Task CanStartBot()
+    {
+        //Arrange.
+        DiscordBot discordBot = new();
+
+        //Act.
+        await discordBot.Execute();
+        bool connected = discordBot.IsConnected();
+
+        //Assert.
+        Assert.True(connected);
+    }
+}
